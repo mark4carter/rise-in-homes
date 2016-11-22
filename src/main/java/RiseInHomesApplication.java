45f3e644da5d64fac006ae4d35244fba89dd9c8b
mark4carter <mark4carter@gmail.com>
2016-11-22 06:41:18 -0500

    transferring to spring

---
+++ src/main/java/RiseInHomesApplication.java
@@ -0,0 +1,21 @@
+package main.java;
+
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiseInHomesApplication implements CommandLineRunner{
+
+  public static void main(String[] args) {
+    SpringApplication.run(RiseInHomesApplication.class, args);
+  }
+
+  @Override
+  public void run(String... arg0) throws Exception {
+    // TODO Auto-generated method stub
+    
+  }
+}
